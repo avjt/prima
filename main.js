5fc6a5ef4d8bc104df160b3f9176805c7a6c3fc5
avjt <avijit.ghosh@aricent.com>
2016-08-12 14:39:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 var http = require("http");
 http.createServer(function (request, response) {
-
    response.writeHead(200, {'Content-Type': 'text/plain'});
-      response.end('Hello World\n');
-}).listen(80);
+   response.end('Hello World\n');
+}).listen(0);
 
-console.log('Server running at http://127.0.0.1:80/');
+// console.log('Server running at http://127.0.0.1:80/');
